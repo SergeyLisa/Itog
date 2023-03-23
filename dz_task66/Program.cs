48dac8ac3247e92f47f22f4d95c7ed13598c9fe0
Sergey <Sergey@yqndex.ru>
2023-03-23 11:43:26 +0300

    all

---
+++ dz_task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int NumbersSum(int m, int n)
+{
+  if (m > n) return 0;
+  else
+  {
+    return m + NumbersSum(m + 1, n);
+  }
+}
+Console.Clear();
+Console.Write("Введите число от: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число до: ");
+int n = int.Parse(Console.ReadLine()!);
+int sum = NumbersSum(m, n);
+Console.Write(sum);
---
+++ dz_task68/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите начальное число M:");
+int numM = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите начальное число N:");
+int numN = int.Parse(Console.ReadLine()!);
+
+int AckFun (int numM, int numN)
+{
+    if (numM == 0) return numN + 1;
+    if (numN == 0) return AckFun(numM - 1, 1);
+    if (numM > 0 && numN > 0) return AckFun(numM - 1, AckFun(numM, numN - 1));
+return AckFun(numM, numN);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({numM},{numN}) = {AckFun(numM, numN)}");
